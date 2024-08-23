63ffa628ec4f75831a0078c3be6a3c77fb691169
Lu, Kuang-Chen <kuang-chen_lu@brown.edu>
2024-08-23 09:07:55 -0400

    translate JS

--- test/translate_test_programs.js
+++ test/translate_test_programs.js
@@ -48,62 +48,62 @@ for (const f of fs.readdirSync(path)) {
             console.error(err);
         }
         // PY
-        try {
-            if (!incompatibleWithPY.has(name.replace(/[.]again$/, ""))) {
-                const program = fs.readFileSync(programFile, 'utf8');
-                const outputs = fs.readFileSync(outputsFile, 'utf8');
-                if (!outputs.includes("@")) {
-                    try {
-                        fs.writeFileSync(`${path}/${name}.py`, SMoL.PYTranslator.translateProgram(true, program));
-                        fs.writeFileSync(`${path}/${name}.py.txt`, SMoL.PYTranslator.translateOutput(outputs));
-                    } catch (err) {
-                        fs.writeFileSync(`${path}/${name}.py.err`, `An error occurred in translation:\n${JSON.stringify(err)}\n${err.toString()}`);
-                    }
-                } else {
-                    fs.writeFileSync(`${path}/${name}.py.err`, "Skipped translation because the outputs include `@`.");
-                }
-            }
-        } catch (err) {
-            console.log(name);
-            console.error(err);
-        }
-        // Pseudocode
-        try {
-            const program = fs.readFileSync(programFile, 'utf8');
-            const outputs = fs.readFileSync(outputsFile, 'utf8');
-            if (!outputs.includes("@")) {
-                try {
-                    fs.writeFileSync(`${path}/${name}.cm`, SMoL.PCTranslator.translateProgram(true, program));
-                    fs.writeFileSync(`${path}/${name}.cm.txt`, SMoL.PCTranslator.translateOutput(outputs));
-                } catch (err) {
-                    fs.writeFileSync(`${path}/${name}.cm.err`, `An error occurred in translation:\n${JSON.stringify(err)}\n${err.toString()}`);
-                }
-            } else {
-                fs.writeFileSync(`${path}/${name}.cm.err`, "Skipped translation because the outputs include `@`.");
-            }
-        } catch (err) {
-            console.log(name);
-            console.error(err);
-        }
-        // Scala
-        try {
-            if (!incompatibleWithScala3.has(name.replace(/[.]again$/, ""))) {
-                const program = fs.readFileSync(programFile, 'utf8');
-                const outputs = fs.readFileSync(outputsFile, 'utf8');
-                if (!outputs.includes("@")) {
-                    try {
-                        fs.writeFileSync(`${path}/${name}.scala`, Scala3_actual_hard_coded_translation[name] || SMoL.SCTranslator.translateProgram(true, program));
-                        fs.writeFileSync(`${path}/${name}.scala.txt`, SMoL.SCTranslator.translateOutput(outputs));
-                    } catch (err) {
-                        fs.writeFileSync(`${path}/${name}.scala.err`, `An error occurred in translation:\n${JSON.stringify(err)}\n${err.toString()}`);
-                    }
-                } else {
-                    fs.writeFileSync(`${path}/${name}.scala.err`, "Skipped translation because the outputs include `@`.");
-                }
-            }
-        } catch (err) {
-            console.log(name);
-            console.error(err);
-        }
+        // try {
+        //     if (!incompatibleWithPY.has(name.replace(/[.]again$/, ""))) {
+        //         const program = fs.readFileSync(programFile, 'utf8');
+        //         const outputs = fs.readFileSync(outputsFile, 'utf8');
+        //         if (!outputs.includes("@")) {
+        //             try {
+        //                 fs.writeFileSync(`${path}/${name}.py`, SMoL.PYTranslator.translateProgram(true, program));
+        //                 fs.writeFileSync(`${path}/${name}.py.txt`, SMoL.PYTranslator.translateOutput(outputs));
+        //             } catch (err) {
+        //                 fs.writeFileSync(`${path}/${name}.py.err`, `An error occurred in translation:\n${JSON.stringify(err)}\n${err.toString()}`);
+        //             }
+        //         } else {
+        //             fs.writeFileSync(`${path}/${name}.py.err`, "Skipped translation because the outputs include `@`.");
+        //         }
+        //     }
+        // } catch (err) {
+        //     console.log(name);
+        //     console.error(err);
+        // }
+        // // Pseudocode
+        // try {
+        //     const program = fs.readFileSync(programFile, 'utf8');
+        //     const outputs = fs.readFileSync(outputsFile, 'utf8');
+        //     if (!outputs.includes("@")) {
+        //         try {
+        //             fs.writeFileSync(`${path}/${name}.cm`, SMoL.PCTranslator.translateProgram(true, program));
+        //             fs.writeFileSync(`${path}/${name}.cm.txt`, SMoL.PCTranslator.translateOutput(outputs));
+        //         } catch (err) {
+        //             fs.writeFileSync(`${path}/${name}.cm.err`, `An error occurred in translation:\n${JSON.stringify(err)}\n${err.toString()}`);
+        //         }
+        //     } else {
+        //         fs.writeFileSync(`${path}/${name}.cm.err`, "Skipped translation because the outputs include `@`.");
+        //     }
+        // } catch (err) {
+        //     console.log(name);
+        //     console.error(err);
+        // }
+        // // Scala
+        // try {
+        //     if (!incompatibleWithScala3.has(name.replace(/[.]again$/, ""))) {
+        //         const program = fs.readFileSync(programFile, 'utf8');
+        //         const outputs = fs.readFileSync(outputsFile, 'utf8');
+        //         if (!outputs.includes("@")) {
+        //             try {
+        //                 fs.writeFileSync(`${path}/${name}.scala`, Scala3_actual_hard_coded_translation[name] || SMoL.SCTranslator.translateProgram(true, program));
+        //                 fs.writeFileSync(`${path}/${name}.scala.txt`, SMoL.SCTranslator.translateOutput(outputs));
+        //             } catch (err) {
+        //                 fs.writeFileSync(`${path}/${name}.scala.err`, `An error occurred in translation:\n${JSON.stringify(err)}\n${err.toString()}`);
+        //             }
+        //         } else {
+        //             fs.writeFileSync(`${path}/${name}.scala.err`, "Skipped translation because the outputs include `@`.");
+        //         }
+        //     }
+        // } catch (err) {
+        //     console.log(name);
+        //     console.error(err);
+        // }
     }
 }
